e86c798f76d5137407f4a840e909abe0f12d47bb
KTE <KTE@DESKTOP-U2Q9FPN>
2021-11-25 12:18:30 +0900

    11.25 Test

---
+++ ex01/src/main/java/org/zerock/domain/SampleDTO.java
@@ -0,0 +1,18 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+	private String name;
+	private int age;
+	
+	
+//	@Override
+//	public String toString() {
+//		return "SampleDTO [name=" + name + ", age=" + age + "]";
+//	}
+//	
+	
+	
+}
